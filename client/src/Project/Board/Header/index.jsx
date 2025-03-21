1989283f9fc02a71d1e025b156afe902f6379503
danieldellgadoo <danieldellgadoo82@gmail.com>
2025-03-20 22:13:32 -0500

    chore: header

--- client/src/Project/Board/Header/index.jsx
+++ client/src/Project/Board/Header/index.jsx
@@ -6,7 +6,7 @@ import { Header, BoardName } from './Styles';
 
 const ProjectBoardHeader = () => (
   <Header>
-    <BoardName>Kanban board</BoardName>
+    <BoardName>Task board</BoardName>
     <a href="https://github.com/oldboyxx/jira_clone" target="_blank" rel="noreferrer noopener">
       <Button icon="github">Github Repo</Button>
     </a>
